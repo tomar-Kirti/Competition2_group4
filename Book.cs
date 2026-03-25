e96b1a9fc74b0058144bc88a27dbf178adf57b85
ayabarkusf <bark@usf.edu>
2026-03-25 11:15:23 -0400

    Book Commit

---
+++ Book.cs
@@ -0,0 +1,34 @@
+﻿namespace LibraryManagementSystem
+{
+    public class Book
+    {
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public string ISBN { get; set; }
+        public int AvailableCopies { get; set; }
+
+        public Book(string title, string author, string isbn, int availableCopies)
+        {
+            Title = title;
+            Author = author;
+            ISBN = isbn;
+            AvailableCopies = availableCopies;
+        }
+
+        public bool BorrowBook()
+        {
+            if (AvailableCopies > 0)
+            {
+                AvailableCopies--;
+                return true;
+            }
+
+            return false;
+        }
+
+        public void ReturnBook()
+        {
+            AvailableCopies++;
+        }
+    }
+}
